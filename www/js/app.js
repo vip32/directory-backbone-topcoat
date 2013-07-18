5f363ec2d6ecfc63cc21d84a5c867ae2d9c6eb2b
Vincent van Proosdij <mini32@gmail.com>
2013-07-18 15:08:24 +0200

    test6

--- www/js/app.js
+++ www/js/app.js
@@ -8,9 +8,10 @@ var app = {
 
 $(document).on("ready", function () {
     app.router = new app.routers.AppRouter();
-    app.utils.templates.load(["HomeView", "EmployeeView", "EmployeeListItemView", "ReportsView", "MapView"],
-        function () {
+    app.utils.templates.load(["HomeView",  "EmployeeView", "EmployeeListItemView", "ReportsView", "MapView"],
+        function ()  {
             app.router = new app.routers.AppRouter();
-            Backbone.history.start();
+             Backbone.history.start();
         });
 });
+   
