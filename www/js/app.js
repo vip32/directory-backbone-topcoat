06fceee166fd14c65620a50e210c9db0039d4580
Vincent van Proosdij <mini32@gmail.com>
2013-07-18 15:36:14 +0200

    cordova 2.8

--- www/js/app.js
+++ www/js/app.js
@@ -3,15 +3,27 @@ var app = {
     models: {},
     routers: {},
     utils: {},
-    adapters: {}
-};
+    adapters: {},
 
-$(document).on("ready", function () {
-    app.router = new app.routers.AppRouter();
-    app.utils.templates.load(["HomeView",  "EmployeeView", "EmployeeListItemView", "ReportsView", "MapView"],
+    initialize: function() {
+        console.log('app initialize');
+        this.bind();
+    },
+    bind: function() {
+        document.addEventListener('deviceready', this.deviceready, false);
+    },
+    deviceready: function() {
+        console.log('device ready');
+        app.router = new app.routers.AppRouter();
+        app.utils.templates.load(["HomeView",  "EmployeeView", "EmployeeListItemView", "ReportsView", "MapView"],
         function ()  {
             app.router = new app.routers.AppRouter();
              Backbone.history.start();
         });
+    },
+};
+
+$(document).on("ready", function () {
+    console.log('document ready');
+    app.initialize();
 });
-   
